637984f1521b51df76d2daa512b485ac99cb7f65
Mershelle Rivera <mershellemae@gmail.com>
2022-09-16 11:39:29 -0700

    java servlet using tomcat javax

---
+++ src/main/java/org/example/FirstServlet.java
@@ -0,0 +1,17 @@
+package org.example;
+
+//import jakarta.servlet.http.HttpServlet;
+//import jakarta.servlet.http.HttpServletRequest;
+//import jakarta.servlet.http.HttpServletResponse;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class FirstServlet extends HttpServlet {
+    public void doGet(HttpServletRequest httpServletRequest,
+                      HttpServletResponse httpServletResponse) throws IOException {
+        httpServletResponse.getWriter().print("Hello from servlet");
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,7 @@
+<html>
+    <body>
+        <h2>Hello World!</h2>
+
+        <a href="FirstServlet">Click here to see servlet</a>
+    </body>
+</html>
