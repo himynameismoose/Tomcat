45bd45fc319c7f2ca6590c70e296ac6e3804ab92
Mershelle Rivera <mershellemae@gmail.com>
2022-09-29 15:08:42 -0700

    run servlet

--- src/main/java/org/example/FirstServlet.java
+++ src/main/java/org/example/FirstServlet.java
@@ -8,10 +8,21 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class FirstServlet extends HttpServlet {
+    @Override
     public void doGet(HttpServletRequest httpServletRequest,
                       HttpServletResponse httpServletResponse) throws IOException {
-        httpServletResponse.getWriter().print("Hello from servlet");
+//        httpServletResponse.getWriter().print("Hello from servlet");
+        PrintWriter out = httpServletResponse.getWriter();
+        httpServletResponse.setContentType("text/html");
+        out.print("<html><head>");
+        out.print("<title>My First Servlet </title>");
+        out.print("</head>");
+        out.print("<body>");
+        out.print("<h1>Hii Welcome to the world of Servlets</h1>");
+        out.print("</body>");
+        out.print("</head>");
     }
 }
